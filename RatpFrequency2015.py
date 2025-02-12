96c293fc299c8b3bd1d702a9cbea3aeb10d30107
flotr18 <trouilletflorent@outlook.com>
2025-02-12 18:47:59 +0100

    Calculated Trafic in 2015 for RER & Subways

--- RatpFrequency2015.py
+++ RatpFrequency2015.py
@@ -3,8 +3,20 @@ import json
 with open('trafic-annuel-entrant-par-station-du-reseau-ferre-2015.json','r') as file:
     data = json.load(file)
     TotalTrafic = 0
+    TotalMetro = 0
+    TotalRER = 0
     for subway in data:
-        TotalTrafic += subway['trafic']
+        if subway['reseau'] == 'RER':
+            TotalRER += subway['trafic']
+        elif subway['reseau'] == 'M\u00e9tro':
+            TotalMetro += subway['trafic']
+        TotalTrafic = TotalMetro + TotalRER
+    print(TotalRER)
+    print(TotalMetro)
     print(TotalTrafic)
+
+        
+        
+
        
 
