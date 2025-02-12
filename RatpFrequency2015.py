fcf036d13101c660cacbdecdb01ef276b804bb43
flotr18 <trouilletflorent@outlook.com>
2025-02-12 15:36:12 +0100

    API CALL from RATP2015

--- RatpFrequency2015.py
+++ RatpFrequency2015.py
@@ -1,4 +1,24 @@
-def hello():
-    print("Hello World")
+import requests as req
 
-hello()
+def GetRatpFrequency2015():
+    url = "https://data.ratp.fr/api/explore/v2.1/catalog/datasets/trafic-annuel-entrant-par-station-du-reseau-ferre-2015/records?limit=100"
+
+    try:
+        res = req.get(url)
+
+        if res.status_code == 200:
+            freq = res.json()
+            for station in freq:
+                print(station)
+        else:
+            print("Error", res.status_code)
+            return None
+    except req.exceptions.RequestException as e:
+  
+        print('Error:', e)
+        return None
+
+        
+
+
+GetRatpFrequency2015()
