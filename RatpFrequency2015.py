f959682a8b067231feb7f611340ee0935fa54837
flotr18 <trouilletflorent@outlook.com>
2025-02-12 16:48:16 +0100

    Set up to normal y axis

--- RatpFrequency2015.py
+++ RatpFrequency2015.py
@@ -2,7 +2,7 @@ import requests as req
 import random
 import matplotlib.pyplot as plt
 
-plt.figure(figsize=(20, 6))
+plt.figure(figsize=(40, 6))
 
 def GetRatpFrequency2015():
     url = "https://data.ratp.fr/api/explore/v2.1/catalog/datasets/trafic-annuel-entrant-par-station-du-reseau-ferre-2015/records?limit=100"
@@ -23,14 +23,15 @@ def GetRatpFrequency2015():
             StationName = results[i]['station']
             StationTrafic = results[i]["trafic"]
             print(results[i]["trafic"])
-            plt.bar(StationName, StationTrafic, label="Trafic")
+            plt.bar(StationName, StationTrafic)
 
-        plt.xlabel("StationName")   
-        plt.ylabel("Points")
+        plt.xlabel("Station Name")   
+        plt.ylabel("Traffic")
 
-        plt.title("F1 24 Constructor Standings after Round 24")
+        plt.ticklabel_format(style="plain", axis="y")
+        plt.title("Trafic from 20 random stations RER/Metro")
         plt.legend()
-        plt.grid(True)
+        plt.grid(False)
         plt.show()        
         
 
