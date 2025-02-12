4dab801f5bce7ea72cc647136b59ee9f94752a68
flotr18 <trouilletflorent@outlook.com>
2025-02-12 18:08:10 +0100

    Calculate Global trafic

--- RatpFrequency2015.py
+++ RatpFrequency2015.py
@@ -2,5 +2,9 @@ import json
 
 with open('trafic-annuel-entrant-par-station-du-reseau-ferre-2015.json','r') as file:
     data = json.load(file)
+    TotalTrafic = 0
     for subway in data:
-        print(subway['rang'])
+        TotalTrafic += subway['trafic']
+    print(TotalTrafic)
+       
+
