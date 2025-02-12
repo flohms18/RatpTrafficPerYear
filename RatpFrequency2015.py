a1912f7af0b1f803b33123cafa237ca47b11f540
flotr18 <trouilletflorent@outlook.com>
2025-02-12 19:03:21 +0100

    Update RatpFrequency2015.py

--- RatpFrequency2015.py
+++ RatpFrequency2015.py
@@ -8,12 +8,16 @@ with open('trafic-annuel-entrant-par-station-du-reseau-ferre-2015.json','r') as 
     for subway in data:
         if subway['reseau'] == 'RER':
             TotalRER += subway['trafic']
+            if len(str(TotalRER)) <= 2:
+                print("Hello")
         elif subway['reseau'] == 'M\u00e9tro':
             TotalMetro += subway['trafic']
         TotalTrafic = TotalMetro + TotalRER
-    print(TotalRER)
-    print(TotalMetro)
-    print(TotalTrafic)
+
+    print(f"Trafic en 2015 - RER:  {TotalRER}")
+    print(f"Trafic en 2015 - Métro:  {TotalMetro}")
+    print(f"Total Trafic:  {TotalTrafic}")
+
 
         
         
