7100c6e8c7019beb35898da7df295cb39ec0cfd5
flotr18 <trouilletflorent@outlook.com>
2025-02-12 19:15:35 +0100

    Add Year 2016

--- RatpFrequency2015.py
+++ RatpFrequency2015.py
@@ -1,22 +1,26 @@
 import json
 
-with open('trafic-annuel-entrant-par-station-du-reseau-ferre-2015.json','r') as file:
-    data = json.load(file)
-    TotalTrafic = 0
-    TotalMetro = 0
-    TotalRER = 0
-    for subway in data:
-        if subway['reseau'] == 'RER':
-            TotalRER += subway['trafic']
-        elif subway['reseau'] == 'M\u00e9tro':
-            TotalMetro += subway['trafic']
-        TotalTrafic = TotalMetro + TotalRER
+Years = [2015,2016]
 
-    print(f"Trafic en 2015 - RER:  {TotalRER}")
-    print(f"Trafic en 2015 - Métro:  {TotalMetro}")
-    print(f"Total Trafic:  {TotalTrafic}")
+for check in Years:
+    filename = f'trafic-annuel-entrant-par-station-du-reseau-ferre-{check}.json'    
+    with open(filename, 'r') as file:        
+        data = json.load(file)
+        TotalTrafic = 0
+        TotalMetro = 0
+        TotalRER = 0
+        for subway in data:
+            if subway['reseau'] == 'RER':
+                TotalRER += subway['trafic']
+            elif subway['reseau'] == 'M\u00e9tro':
+                TotalMetro += subway['trafic']
+            TotalTrafic = TotalMetro + TotalRER
 
+        print(f"Trafic en {check} - RER:  {TotalRER}")
+        print(f"Trafic en {check} - Métro:  {TotalMetro}")
+        print(f"Total Trafic en {check}:  {TotalTrafic}")
 
+import json
         
         
 
