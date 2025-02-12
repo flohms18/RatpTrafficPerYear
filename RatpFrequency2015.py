c8fb9019883d3aa4ee6c80a3df98037ada40bd42
flotr18 <trouilletflorent@outlook.com>
2025-02-12 16:42:00 +0100

    Creted Graphic

--- RatpFrequency2015.py
+++ RatpFrequency2015.py
@@ -1,8 +1,11 @@
 import requests as req
 import random
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(20, 6))
 
 def GetRatpFrequency2015():
-    url = "https://data.ratp.fr/api/explore/v2.1/catalog/datasets/trafic-annuel-entrant-par-station-du-reseau-ferre-2015/records"
+    url = "https://data.ratp.fr/api/explore/v2.1/catalog/datasets/trafic-annuel-entrant-par-station-du-reseau-ferre-2015/records?limit=100"
     
     SelectedStations = []
     while len(SelectedStations) <= 20:
@@ -17,6 +20,19 @@ def GetRatpFrequency2015():
         for i in SelectedStations:
             results = data.get("results", [])
             print(results[i]['station'])
+            StationName = results[i]['station']
+            StationTrafic = results[i]["trafic"]
+            print(results[i]["trafic"])
+            plt.bar(StationName, StationTrafic, label="Trafic")
+
+        plt.xlabel("StationName")   
+        plt.ylabel("Points")
+
+        plt.title("F1 24 Constructor Standings after Round 24")
+        plt.legend()
+        plt.grid(True)
+        plt.show()        
+        
 
         
             
