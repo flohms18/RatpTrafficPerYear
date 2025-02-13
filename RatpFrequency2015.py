8c8688ccd218ee56587de0ddc52fe589d354c7fa
flotr18 <trouilletflorent@outlook.com>
2025-02-13 11:26:56 +0100

    Metro & RER in 2017

--- RatpFrequency2015.py
+++ RatpFrequency2015.py
@@ -10,13 +10,19 @@ for check in Years:
     with open(filename, 'r') as file:        
         data = json.load(file)
         TotalRER2017 = 0
+        TotalMetro2017 = 0
         TotalTrafic = 0
         TotalMetro = 0
         TotalRER = 0
         for subway in data:
             if subway['reseau'] == 'RER' and check == 2017:
                 TotalRER2017 += subway['trafic']
-        print(f"Trafic en {check} - RER:  {TotalRER2017}")
+            elif subway['reseau'] == 'M\u00e9tro' and check == 2017:
+                TotalMetro2017 += subway['trafic']
+        print(f"Trafic en 2017 - RER:  {TotalRER2017}")
+        print(f"Trafic en 2017 - Metro: {TotalMetro2017}")
+
+        
 
        
 
