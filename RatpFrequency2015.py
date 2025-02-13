14936686b4f67778a7d573e18b084b319e8e7c11
flotr18 <trouilletflorent@outlook.com>
2025-02-13 11:35:15 +0100

    Update RatpFrequency2015.py

--- RatpFrequency2015.py
+++ RatpFrequency2015.py
@@ -9,11 +9,12 @@ for check in Years:
     filename = f'./trafic-json-ratp/trafic-annuel-entrant-par-station-du-reseau-ferre-{check}.json'    
     with open(filename, 'r') as file:        
         data = json.load(file)
-        TotalRER2017 = 0
-        TotalMetro2017 = 0
-        TotalTrafic = 0
-        TotalMetro = 0
-        TotalRER = 0
+        TotalRER2015, TotalMetro2015 = 0
+        TotalRER2016, TotalMetro2016 = 0
+        TotalRER2017, TotalMetro2017 = 0
+        TotalRER2018, TotalMetro2018 = 0
+        TotalRER2019, TotalMetro2019 = 0
+        TotalRER2020, TotalMetro2020 = 0
         for subway in data:
             if subway['reseau'] == 'RER' and check == 2017:
                 TotalRER2017 += subway['trafic']
