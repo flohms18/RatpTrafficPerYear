555af049d0191e3d7db310b330dd5aa94f7bd726
flotr18 <trouilletflorent@outlook.com>
2025-02-13 14:56:01 +0100

    Improved Code

--- RatpFrequency2015.py
+++ RatpFrequency2015.py
@@ -1,28 +1,38 @@
 import json
 import matplotlib.pyplot as plt
 
-Years = [2015,2016,2017,2018,2019,2020]
+Total = {}
 
-plt.figure(figsize=(20, 6))
-
-for check in Years:
-    filename = f'./trafic-json-ratp/trafic-annuel-entrant-par-station-du-reseau-ferre-{check}.json'    
-    with open(filename, 'r') as file:        
+for i in range(2015, 2021):
+    Total[i] = {  
+        'metro': 0,
+        'rer': 0,
+        'year' : i
+    }
+    check = i
+    filename = f'./trafic-json-ratp/trafic-annuel-entrant-par-station-du-reseau-ferre-{check}.json'
+    with open(filename,'r') as file:
         data = json.load(file)
-        totaux = {annee: {"RER": 0, "Metro": 0} for annee in range(2015, 2020)}
-        TotalRER2015, TotalMetro2015 = 0
-        TotalRER2016, TotalMetro2016 = 0
-        TotalRER2017, TotalMetro2017 = 0
-        TotalRER2018, TotalMetro2018 = 0
-        TotalRER2019, TotalMetro2019 = 0
-        TotalRER2020, TotalMetro2020 = 0
         for subway in data:
-            if subway['reseau'] == 'RER' and check == 2017:
-                TotalRER2017 += subway['trafic']
-            elif subway['reseau'] == 'M\u00e9tro' and check == 2017:
-                TotalMetro2017 += subway['trafic']
-        print(f"Trafic en 2017 - RER:  {TotalRER2017}")
-        print(f"Trafic en 2017 - Metro: {TotalMetro2017}")
+            if subway['reseau'] == 'RER':
+                Total[i]['rer'] += subway['trafic']
+            elif subway['reseau'] == 'M\u00e9tro': 
+                Total[i]['metro'] += subway['trafic']
+        print(f"Trafic Métro en {check}: {Total[i]['metro']}")
+        print(f"Trafic RER en {check}: {Total[i]['rer']}")
+
+
+
+
+
+
+    
+
+
+
+    
+
+
 
         
 
