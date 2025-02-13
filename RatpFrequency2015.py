c2a4fe4e001205c0226648806b8ebd149ba7738d
flotr18 <trouilletflorent@outlook.com>
2025-02-13 11:06:26 +0100

    Update RatpFrequency2015.py

--- RatpFrequency2015.py
+++ RatpFrequency2015.py
@@ -17,10 +17,9 @@ for check in Years:
             TotalTrafic = TotalMetro + TotalRER
 
         print(f"Trafic en {check} - RER:  {TotalRER}")
-        print(f"Trafic en {check} - Métro:  {TotalMetro}")
+        print(f"Trafic en {check} - Métro: {TotalMetro}")
         print(f"Total Trafic en {check}:  {TotalTrafic}")
 
-import json
         
         
 
