b460ec54311c237a47d65e98827505c93739e231
flotr18 <trouilletflorent@outlook.com>
2025-02-13 11:19:30 +0100

    Get Trafic RER 2017

--- RatpFrequency2015.py
+++ RatpFrequency2015.py
@@ -1,27 +1,27 @@
 import json
+import matplotlib.pyplot as plt
 
 Years = [2015,2016,2017,2018,2019,2020]
+
+plt.figure(figsize=(20, 6))
 
 for check in Years:
     filename = f'./trafic-json-ratp/trafic-annuel-entrant-par-station-du-reseau-ferre-{check}.json'    
     with open(filename, 'r') as file:        
         data = json.load(file)
+        TotalRER2017 = 0
         TotalTrafic = 0
         TotalMetro = 0
         TotalRER = 0
         for subway in data:
-            if subway['reseau'] == 'RER':
-                TotalRER += subway['trafic']
-            elif subway['reseau'] == 'M\u00e9tro':
-                TotalMetro += subway['trafic']
-            TotalTrafic = TotalMetro + TotalRER
+            if subway['reseau'] == 'RER' and check == 2017:
+                TotalRER2017 += subway['trafic']
+        print(f"Trafic en {check} - RER:  {TotalRER2017}")
 
-        print(f"Trafic en {check} - RER:  {TotalRER}")
-        print(f"Trafic en {check} - Métro: {TotalMetro}")
-        print(f"Total Trafic en {check}:  {TotalTrafic}")
+       
 
-        
-        
+
+
 
        
 
